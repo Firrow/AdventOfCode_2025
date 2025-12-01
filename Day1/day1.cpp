73f22d226c1e81927abe036bb289dda014def298
Firrow <73218766+Firrow@users.noreply.github.com>
2025-12-01 20:40:14 +0100

    Open file

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -2,18 +2,42 @@
 #include <fstream>
 #include <string>
 
+
+
 int LineParse(std::string element)
 {
     int result = std::stoi(element.substr(1, element.length()));
     return element.substr(0, 1) == "L" ? result*-1 : result;
 }
 
+int ReadFile(std::string fileName, int start)
+{
+    int temp = start;
+    std::ifstream file(fileName);
+    std::string line;
+    
+    if(file.is_open()) {
+        while(getline(file, line)) {
+            std::cout << line << '\n';
+            //parse
+            //calcul previous + line
+            //check if +1 or not
+        }
+        file.close();
+    } else {
+        std::cout << "Impossible d'ouvrir le fichier." << std::endl;
+    }
+    
+    return 0;
+}
+
+
+
 int main()
 {
-    std::cout << LineParse("L52") << std::endl;
-    std::cout << LineParse("R52") << std::endl;
-    std::cout << LineParse("L522") << std::endl;
-    std::cout << LineParse("R522") << std::endl;
-    std::cout << LineParse("L2") << std::endl;
-    std::cout << LineParse("R2") << std::endl;
+    std::string filePath; 
+    std::cout << "Enter the input file: ";
+    std::cin >> filePath;
+
+    ReadFile(filePath, 0);
 }
