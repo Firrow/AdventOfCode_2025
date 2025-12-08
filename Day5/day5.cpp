89e75dc54ad9a9d735d63bd17d7a6a242679c26f
Firrow <73218766+Firrow@users.noreply.github.com>
2025-12-08 14:03:53 +0100

    day5 in progress

--- Day5/day5.cpp
+++ Day5/day5.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <vector>
 #include <chrono>
+#include <algorithm>
 
 using namespace std::chrono;
 
@@ -33,8 +34,26 @@ int ReadFile(std::string _fileName, std::vector<std::string>& outIdsRanges, std:
     return 0;
 }
 
+void ConvertRanges(std::vector<std::string>& _IdsRangesInFile, std::vector<std::pair<std::int64_t, std::int64_t>>& _IdsRanges)
+{
+    for (std::string range : _IdsRangesInFile)
+    {
+        size_t pos = range.find('-');
+        assert(pos != std::string::npos); //npos = -1
 
+        std::int64_t from = std::stoll(range.substr(0, pos));
+        std::int64_t to = std::stoll(range.substr(pos + 1));
 
+        _IdsRanges.push_back(std::make_pair(from, to));
+    }
+}
+
+void SortRangesByFirstElement(std::vector<std::pair<std::int64_t, std::int64_t>>& _IdsRanges)
+{
+    std::sort(_IdsRanges.begin(), _IdsRanges.end());
+}
+
+//Part1
 int CountIDInRange(std::vector<std::string>& _IdsRanges, std::vector<std::string>& _IdsToProcess)
 {
     int count = 0;
@@ -65,6 +84,49 @@ int CountIDInRange(std::vector<std::string>& _IdsRanges, std::vector<std::string
     return count;
 }
 
+std::vector<std::pair<std::int64_t, std::int64_t>> SimplifyIntervals(std::vector<std::pair<std::int64_t, std::int64_t>>& _IdsRanges)
+{
+    std::vector<std::pair<std::int64_t, std::int64_t>> newIdsRanges;
+    std::int64_t previousTo = 0;
+    std::int64_t currentFrom = -1;
+    std::int64_t currentTo = -1;
+
+    for (size_t i = 0; i < _IdsRanges.size(); i++)
+    {
+        currentFrom = _IdsRanges[i].first;
+        currentTo = _IdsRanges[i].second;
+
+        std::cout << "currentFrom : " << currentFrom << "\n";
+        std::cout << "currentTo : " << currentTo << "\n";
+
+        if (currentFrom > previousTo + 1) // we create a new interval
+        {
+            newIdsRanges.push_back(std::make_pair(currentFrom, currentTo));
+            std::cout << "Je créer un nouvel interval" << "\n";
+        }
+        else // we modify or not the current interval
+        {
+            if (currentTo >= previousTo)
+            {
+                newIdsRanges.back().second = currentTo;
+                std::cout << "Je mets à jour l'interval" << "\n";
+            }
+        }
+
+        previousTo = currentTo;
+
+        std::cout << " ------------------------------------- " << "\n";
+    }
+    
+    return newIdsRanges;
+}
+
+//Part2
+/*int CountAllValidIDs(std::vector<std::pair<std::int64_t, std::int64_t>>& _IdsRanges)
+{
+
+}*/
+
 
 
 int main()
@@ -73,22 +135,44 @@ int main()
     std::cout << "Enter the input file: ";
     std::cin >> filePath;
 
-    std::vector<std::string> IdsRanges;
-    std::vector<std::string> IdsToProcess;
+    std::vector<std::string> IdsRangesInFile;
+    std::vector<std::string> IdsToProcessInFile;
+    std::vector<std::pair<std::int64_t, std::int64_t>> IdsRanges;
+    std::vector<std::pair<std::int64_t, std::int64_t>> newIdsRanges;
 
     //   ../Inputs/input5.txt
     //   ../Inputs/inputTest.txt
 
     //PART 1 -------------------------------------------------
+    /*
     auto start = high_resolution_clock::now();
-    ReadFile(filePath, IdsRanges, IdsToProcess);
-    int finalResult = CountIDInRange(IdsRanges, IdsToProcess);
+    ReadFile(filePath, IdsRangesInFile, IdsToProcessInFile);
+    int finalResult = CountIDInRange(IdsRangesInFile, IdsToProcessInFile);
 
     std::cout << finalResult << "\n";
     auto stop = high_resolution_clock::now();
 
     auto duration = duration_cast<microseconds>(stop - start);
     std::cout << "EXECUTION TIME (s): " << duration.count() / 1000000.0 << std::endl;
+    */
     //PART 2 -------------------------------------------------
+    auto start = high_resolution_clock::now();
+    ReadFile(filePath, IdsRangesInFile, IdsToProcessInFile);
+    ConvertRanges(IdsRangesInFile, IdsRanges);
+    SortRangesByFirstElement(IdsRanges);
+    newIdsRanges = SimplifyIntervals(IdsRanges);
 
+    for (auto i : newIdsRanges)
+    {
+        std::cout << i.first << " - " << i.second << "\n";
+    }
+    
+
+    //int finalResult = CountIDInRange(IdsRanges, IdsToProcess);
+
+    //std::cout << finalResult << "\n";
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+    std::cout << "EXECUTION TIME (s): " << duration.count() / 1000000.0 << std::endl;
 }
