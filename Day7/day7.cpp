510dea828024ccfdc67b07a7aee4e072dadc2fa5
Firrow <73218766+Firrow@users.noreply.github.com>
2025-12-10 00:06:29 +0100

    init day7

---
+++ Day7/day7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cassert>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+
+using namespace std::chrono;
+
+
+
+int main()
+{
+    std::string filePath; 
+    std::cout << "Enter the input file: ";
+    std::cin >> filePath;
+
+    //   ../Inputs/input7.txt
+    //   ../Inputs/inputTest.txt
+
+    //PART 1 -------------------------------------------------
+    
+    //PART 2 -------------------------------------------------
+}
